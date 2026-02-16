4bcfbd3a29a55809422e66e7cf2a62952dda8c08
unknown <anujtrikha199@gmail.com>
2026-02-16 08:58:45 +0530

    Integrating CI

--- web/app.js
+++ web/app.js
@@ -26,7 +26,6 @@ console.log('App is ready to handle requests!!')
 console.log('App is ready to handle requests!!skh')
 console.log('App is ready to handle requests!!skh')
 
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
