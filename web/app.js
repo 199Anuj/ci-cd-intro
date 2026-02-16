946da10f9b2211f1c04e00b462aeb234675f97ee
unknown <anujtrikha199@gmail.com>
2026-02-16 08:51:09 +0530

    Integrating CI

--- web/app.js
+++ web/app.js
@@ -21,7 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 console.log('App initialized successfully!')
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-console.log('Routes registered successfully!')
+console.log('Routes registered successfully!!')
+console.log('App is ready to handle requests!!')
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
